f32ab041d7e7d3aa5ea4362bf1dc35653cded3cd
styletab <katherinetab@gmail.com>
2016-10-01 17:52:29 -0400

    Setup Auth Files
    Added files for curl scripts and code. No code is currently in the api,
    events, or ui files.

---
+++ assets/curl scripts/auth.sh
@@ -0,0 +1,40 @@
+
+# sign up
+
+curl --include --request POST http://localhost:3000/sign-up \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "an@example.email",
+      "password": "an example password",
+      "password_confirmation": "an example password"
+    }
+  }'
+
+#sign in
+
+curl --include --request POST http://localhost:3000/sign-in \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "an@example.email",
+      "password": "an example password"
+    }
+  }'
+
+#change password
+
+curl --include --request PATCH http://localhost:3000/change-password/$ID \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "an example password",
+      "new": "super sekrit"
+    }
+  }'
+
+#sign out
+
+curl --include --request DELETE http://localhost:3000/sign-out/$ID \
+  --header "Authorization: Token token=$TOKEN"
