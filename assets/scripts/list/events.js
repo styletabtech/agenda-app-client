5e9088955423346f63ddabdd984d2ed77810c17a
styletab <katherinetab@gmail.com>
2016-10-02 18:05:11 -0400

    Full feature POST to /lists/
    Completed POST feature to create a new list on /lists/

--- assets/scripts/list/events.js
+++ assets/scripts/list/events.js
@@ -5,76 +5,19 @@ const getFormFields = require('../../../lib/get-form-fields');
 const api = require('./api');
 const ui = require('./ui');
 
-// for setup
+// for creating a new list
 
 const getNewList = function (event) {
   event.preventDefault();
   ui.proceedToCreateList(event);
-
-
-  // let data = getFormFields(event.target);
-  //   api.createNewList(data)
-  //     .done(ui.newListSuccess)
-  //     .fail(ui.failure);
 };
 
-const getSignedUp = function () {
+const onNewList = function (event) {
   event.preventDefault();
-    ui.proceedToSignUp(event);
-};
-
-// sign up
-
-const onSignUp = function (event) {
-  // console.log('ongSignUp is running');
-  event.preventDefault();
-  let signUpdata = getFormFields(event.target);
-    api.signUp(signUpdata)
-      .done(function(data, textStatus, jqXHR) {
-        api.autoLogIn(data, textStatus, jqXHR, signUpdata)
-      .done(ui.signUpSuccess)
+  let data = getFormFields(event.target);
+    api.createNewList(data)
+      .done(ui.newListSuccess)
       .fail(ui.failure);
-    })
-    .fail(ui.failure);
-
-};
-
-// // login
-
-const onLogIn = function (event) {
-
-  event.preventDefault();
-  let data = getFormFields(event.target);
-//  console.log('data is', data);
-  api.logIn(data)
-  .done(ui.loginSuccess)
-  .fail(ui.failure);
-};
-
-// // change password
-
-const getChangePassword = function (event) {
-  event.preventDefault();
-    ui.proceedToChangePw(event);
-};
-
-const onChangePassword = function (event) {
-  event.preventDefault();
-  let data = getFormFields(event.target);
-  console.log('data in events is', data);
-  api.changePassword(data)
-  .done(ui.changePasswordSuccess)
-  .fail(ui.failure);
-};
-
-// //sign out
-
-const onSignOut =  function (event) {
-  event.preventDefault();
-  let data = getFormFields(event.target);
-  api.signOut(data)
-  .done(ui.signOutSuccess)
-  .fail(ui.failure);
 };
 
 
@@ -82,35 +25,16 @@ const onSignOut =  function (event) {
 
 const addHandlers = () => {
 
-  //for a creating lis
+  //for creating a new list
 
   $('#new-list-button').on('click', getNewList);
-
-
-
-  $('.signup-button').on('click', getSignedUp);
-  $('#sign-up').on('submit', onSignUp);
-
-// sign in
-
- $('#login').on('submit', onLogIn);
-
-// //change pw
-  $('#change-pw-nav').on('click', getChangePassword);
-  $('#change-pw').on('submit', onChangePassword);
-
-// // sign out
-$('#sign-out-nav').on('click', onSignOut);
-
-$(document).on('ready', function(){
-  $('.intro-wrapper').show();
-
-});
+  $('#new-list-form').on('submit', onNewList);
 
 };
 
 module.exports = {
   addHandlers,
   getNewList,
+  onNewList
 
 };
--- assets/scripts/list/ui.js
+++ assets/scripts/list/ui.js
@@ -9,6 +9,20 @@ const proceedToCreateList = () => {
 };
 
 
+const newListSuccess = (data) => {
+  console.log('new list success');
+  app.user = data.user;
+  $('#new-list-form').addClass('hide');
+};
+
+const failure = () => {
+  $('.pw-error').removeClass('hide');
+  setTimeout(function() {
+   $('.pw-error').fadeOut();
+ }, 800);
+};
+
+
 // const proceedToLogin = () => {
 //   $('.intro-wrapper').addClass('hide');
 //   $('.login-wrapper').removeClass('hide');
@@ -17,4 +31,6 @@ const proceedToCreateList = () => {
 
 module.exports = {
   proceedToCreateList,
+  newListSuccess,
+  failure
 };
