4458f119572f22bf1ea64a90262a3a743c646cf7
styletab <katherinetab@gmail.com>
2016-10-04 09:54:15 -0400

    Add grunt build for redeployment

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(26);
+	module.exports = __webpack_require__(47);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 26:
+/***/ 47:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
